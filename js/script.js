72347e9c338d2871890eed614df3d2c0e5fd24c9
Serhiy <sergy200478@gmail.com>
2024-07-14 12:32:30 +0300

    Timer added

--- js/script.js
+++ js/script.js
@@ -1,40 +1,107 @@
 "use strict";
 
-const tabsParent = document.querySelector(".tabheader__items"),
-      tabs = document.querySelectorAll(".tabheader__item"),
-      tabContent = document.querySelectorAll(".tabcontent");
+window.addEventListener("DOMContentLoaded", function() {
 
+    //Tabs
 
-function hideTabsContent() {
-    tabContent.forEach(item => {
-        item.classList.add("hide");
-        item.classList.remove("show");
+    const tabsParent = document.querySelector(".tabheader__items"),
+          tabs = document.querySelectorAll(".tabheader__item"),
+          tabContent = document.querySelectorAll(".tabcontent");
+
+    function hideTabsContent() {
+        tabContent.forEach(item => {
+            item.classList.add("hide");
+            item.classList.remove("show");
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
+
+    function showTabsContent(i) {
+        tabContent[i].classList.remove("hide");
+        tabContent[i].classList.add("show", "fade");
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    hideTabsContent();
+    showTabsContent(0);
+
+    tabsParent.addEventListener("click", e => {
+        const target = e.target;
+
+        if (target && target.classList.contains("tabheader__item")) {
+
+            tabs.forEach((item, i) => {
+                if (item == target) {
+                    hideTabsContent();
+                    showTabsContent(i);
+                }
+            })
+        }        
     });
 
-    tabs.forEach(item => {
-        item.classList.remove("tabheader__item_active");
-    });
-}
+    //Timer
 
-function showTabsContent(i) {
-    tabContent[i].classList.remove("hide");
-    tabContent[i].classList.add("show", "fade");
-    tabs[i].classList.add("tabheader__item_active");
-}
+    const deadline = "2024-08-01";
 
-hideTabsContent();
-showTabsContent(0);
+    function getTimeRemeining(endtime) {
+        let days, hours, minutes, seconds;
+        const total = Date.parse(endtime) - Date.parse(new Date());
 
-tabsParent.addEventListener("click", e => {
-    const target = e.target;
+        if (total <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(total / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((total / 1000 / 60) % 60),
+            seconds = Math.floor((total / 1000) % 60);
+        }
 
-    if (target && target.classList.contains("tabheader__item")) {
+        return {
+            "total": total,
+            "days": days,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds
+        }
+    }
 
-        tabs.forEach((item, i) => {
-            if (item == target) {
-                hideTabsContent();
-                showTabsContent(i);
+    function getZero(item) {
+        if (item >= 0 && item <= 9) {
+            return `0${item}`;
+        } else {
+            return item;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector("#days"),
+              hours = timer.querySelector("#hours"),
+              minutes = timer.querySelector("#minutes"),
+              seconds = timer.querySelector("#seconds"),
+              setTimer = setInterval(runTimer, 1000);
+        
+        runTimer();
+
+        function runTimer() {
+            const t = getTimeRemeining(endtime);
+            
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t <= 0) {
+                clearInterval(setTimer);
             }
-        })
-    }        
-});
+        }
+    }
+
+    setClock(".timer", deadline);
+})
